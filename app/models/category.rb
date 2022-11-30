f9b23ba9df4eea3fa310804c193dfa729380512c
rega1237 <rega1237@gmail.com>
2022-11-30 10:25:24 -0400

    Add attached image to categories and local image to feed the database

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
+  has_one_attached :image
+
   has_many :service_categories, dependent: :destroy
   has_many :services, through: :service_categories
 end
