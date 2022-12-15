08e1c264093acb9da5cfc36d36852ce0be900c2e
rega1237 <rega1237@gmail.com>
2022-12-15 18:45:57 -0400

    create comment scaffold

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,6 +5,7 @@ class Service < ApplicationRecord
   has_one_attached :image, dependent: :destroy
   has_many :service_categories, dependent: :destroy
   has_many :categories, through: :service_categories
+  has_many :comments, dependent: :destroy
 
   accepts_nested_attributes_for :categories, allow_destroy: true
 end
