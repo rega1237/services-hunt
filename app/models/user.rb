f896424eda8047089a6cbddc7d2b1042c3bac3b8
rega1237 <rega1237@gmail.com>
2022-12-17 17:20:07 -0400

    Add avatar to user model and open in strong params update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+  has_one_attached :avatar, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
