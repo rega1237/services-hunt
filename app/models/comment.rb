5beea6fd98065a7553db4f983d6f9279828648d4
rega1237 <rega1237@gmail.com>
2022-12-26 16:24:17 -0400

    add validations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :service
+
+  validates :user_id, presence: true
+  validates :service_id, presence: true
+  validates :rating, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
+  
 end
